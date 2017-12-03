8a276e51530f7020d1752e97f8b3f86bb09ee8ac
Gulin Sergey <sg@ya.ru>
2017-12-03 23:50:50 +0300

    пункт about

--- app/js/common.js
+++ app/js/common.js
@@ -10,7 +10,16 @@ $(document).ready(function(){
   	pager: false
   });
   $('.owl-carousel').owlCarousel({
-  	items:4,
-  	margin: 10
+  	items:3,
+  	margin: 10,
+  	startPosition:1,
+  	nav:false,
+  	navText: ["<img src='../img/carousel/prev.png'>","<img src='../img/carousel/next.png'>"]
+
   });
+
+	$('.owl-carousel').find('.owl-nav').removeClass('disabled');
+	$('.owl-carousel').on('changed.owl.carousel', function(event) {
+		$(this).find('.owl-nav').removeClass('disabled');
 	});
+});
